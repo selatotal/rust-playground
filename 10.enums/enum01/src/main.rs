da8428a7a6713f2d9fcc09038959296901173ce7
Tales Viegas <tales.viegas@ilegra.com>
2019-10-23 16:15:56 -0300

    Enums

--- 10.enums/enum01/src/main.rs
+++ 10.enums/enum01/src/main.rs
@@ -1,19 +1,7 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
-
-let home = IpAddr {
-    kind: IpAddrKind::V4,
-    address: String.from("127.0.0.1"),
-};
-
-let loopback = IpAddr {
-    kind: IpAddrKind::V6,
-    address: String.from("::1"),
-};
+let home = IpAddrKind::V4(String.from("127.0.0.1"));
+let loopback = IpAddrKind::V6(String.from("::1"));
