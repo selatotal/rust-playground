526e8a04985c1c8d510d2a8dfb819af35ea56339
Tales Viegas <tales.viegas@ilegra.com>
2019-10-25 12:55:06 -0300

    Error Handling

---
+++ 13.error_handling/recoverable_4/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+use std::fs;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn main() {
+    let username = read_username_from_file();
+    println!("Result: {:?}", username);
+}
