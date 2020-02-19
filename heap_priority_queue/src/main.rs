032146fac1dec111b5a63507fb19f7d591e65c19
Tales Viegas <tales.viegas@ilegra.com>
2020-02-19 15:39:31 -0300

    heap_priority_queue

---
+++ heap_priority_queue/src/main.rs
@@ -0,0 +1,17 @@
+use priority_queue::PriorityQueue;
+
+fn main() {
+    let mut pq = PriorityQueue::new();
+
+    assert!(pq.is_empty());
+    pq.push("Apples", 5);
+    pq.push("Bananas", 8);
+    pq.push("Strawberries", 23);
+    assert_eq!(pq.peek(), Some((&"Strawberries", &23)));
+
+    println!("Get from: {:?}",pq.get(&"Apples").unwrap());
+    println!("For each... ");
+    for (item, _) in pq.into_sorted_iter() {
+        println!("{}", item);
+    }
+}
