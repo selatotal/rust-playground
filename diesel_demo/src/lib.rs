c451bbb3e4124dc272b7a75aec8b3532c6041eb9
Tales Bitelo Viegas <selatotal@gmail.com>
2020-04-30 11:07:20 -0300

    Diesel demo

---
+++ diesel_demo/src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate diesel;
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub mod schema;
+pub mod models;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
